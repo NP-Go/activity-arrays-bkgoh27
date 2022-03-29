fe13b7b48959d54aab10129e4e64d427bdde63ab
bkgoh27 <bengkoon.goh27@gmail.com>
2022-03-29 15:43:37 +0800

    array string and strconv itoa

--- arrays.go
+++ arrays.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var a [10]int
+	var a [10]string
 	for i := 0; i < len(a); i++ {
-		a[i] = i
+		a[i] = strconv.Itoa(i)
 	}
 	fmt.Println(a)
 }
