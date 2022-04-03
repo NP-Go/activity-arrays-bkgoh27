a4a528441dfa53ad39419044e915b6c6ba8d52b3
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 17:01:26 +0800

    data and its length

--- arrays.go
+++ arrays.go
@@ -11,4 +11,18 @@ func main() {
 		a[i] = strconv.Itoa(i)
 	}
 	fmt.Println(a)
+
+	var data [7]string
+	data[0] = "Operating System List"
+	data[1] = "Windows XP"
+	data[2] = "Linux 1.0"
+	data[3] = "Raspbian Teensy"
+	data[4] = "MacOS"
+	data[5] = "IOS"
+	data[6] = "Google Andriod"
+
+	for i := 0; i < len(data); i++ {
+		fmt.Println(data[i], len(data[i]))
+	}
+
 }
